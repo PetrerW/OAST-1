f00f29b61abcc50f87769e1b99d9d653fa3e81b7
PetrerW <piotrw76@gmail.com>
2019-04-15 17:53:35 +0200

    Close the class

--- src/com/company/RandomGenerator.java
+++ src/com/company/RandomGenerator.java
@@ -12,21 +12,26 @@ public class RandomGenerator {
 
     /**
      * Generates a value from exponential distribution
+     *
      * @return
      */
-    public static double getExp(double lambda){
+    public static double getExp(double lambda) {
         double w = 1 - R.nextDouble();
-        return Math.log(w)/lambda;
+        return Math.log(w) / lambda;
     }
 
     /**
      * nextInt is normally exclusive of the top value,
      * so add 1 to make it inclusive
+     *
      * @param min
      * @param max
      * @return
      */
-    public static double getDouble(double min, double max){
+    public static double getDouble(double min, double max) {
         return ThreadLocalRandom.current().nextDouble(min, max);
 
     }
+
+    
+}
